e45ebe83e9835b51fac435f45637494b373cdd5c
hezronkimutai <hezronchelimo.hc@gmail.com>
2021-01-04 20:34:34 +0300

    updated the main README file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Algorithmns-DataStructures
-Solutions to problems from my hacker rank account
+
+This repo is meant for practice.
+
+From time to time I will be looking for problems from other sites e.g hacker_rank and solving them here.
+
+I will also be copy pasting solution from other sources and playing with them to increase my understanding.
+
+Feel free to star, fork or even contribute to the repo.
+
+CHEERS!
--- sortingAlgorthms/CountingSort.java
+++ sortingAlgorthms/CountingSort.java
@@ -1,49 +1,3 @@
-// import java.util.*;
-
-// class CountingSort {
-
-//   static void countSort(int[] arr) {
-//     int max = Arrays.stream(arr).max().getAsInt();
-//     int min = Arrays.stream(arr).min().getAsInt();
-//     int range = max - min + 1;
-//     int count[] = new int[range];
-//     int output[] = new int[arr.length];
-//     for (int i = 0; i < arr.length; i++) {
-//       count[arr[i] - min]++;
-//     }
-
-//     for (int i = 1; i < count.length; i++) {
-//       count[i] += count[i - 1];
-//     }
-
-//     for (int i = arr.length - 1; i >= 0; i--) {
-//       output[count[arr[i] - min] - 1] = arr[i];
-//       count[arr[i] - min]--;
-//     }
-
-//     for (int i = 0; i < arr.length; i++) {
-//       arr[i] = output[i];
-//     }
-//   }
-
-//   static void printArray(int[] arr) {
-//     for (int i = 0; i < arr.length; i++) {
-//       System.out.print(arr[i] + " ");
-//     }
-//     System.out.println("");
-//   }
-
-//   public static void main(String[] args) {
-//     int[] arr = { -5, -10, 0, -3, 8, 5, -1, 10 };
-//     System.out.println("Before Sort");
-//     printArray(arr);
-//     countSort(arr);
-//     System.out.println("After Sort");
-//     printArray(arr);
-//   }
-// }
-// Counting sort in Java programming
-
 import java.util.Arrays;
 
 class CountingSort {
@@ -57,38 +11,22 @@ class CountingSort {
       if (array[i] > max) max = array[i];
     }
     int[] count = new int[max + 1];
-    System.out.println();
-    for (int i = 0; i < array.length; i++) {
-      System.out.print(array[i] + " ");
-    }
 
     // Initialize count array with all zeros.
     for (int i = 0; i < max; ++i) {
       count[i] = 0;
-    }
-    System.out.println();
-    for (int i = 0; i < count.length; i++) {
-      System.out.print(count[i] + " ");
     }
 
     // Store the count of each element
     for (int i = 0; i < size; i++) {
       count[array[i]]++;
     }
-    System.out.println();
-    for (int i = 0; i < count.length; i++) {
-      System.out.print(count[i] + " ");
-    }
-    System.out.println();
 
     // Store the cummulative count of each array
     for (int i = 1; i <= max; i++) {
       count[i] += count[i - 1];
     }
-    for (int i = 0; i < count.length; i++) {
-      System.out.print(count[i] + " ");
-    }
-    System.out.println();
+
     // Find the index of each element of the original array in count array, and
     // place the elements in output array
     for (int i = size - 1; i >= 0; i--) {
