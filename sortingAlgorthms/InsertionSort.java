4a3e6a8a36e486c11d92bd3750f5f2731773f923
hezronkimutai <hezronchelimo.hc@gmail.com>
2021-01-05 08:52:51 +0300

    insertion sort docs

--- sortingAlgorthms/InsertionSort.java
+++ sortingAlgorthms/InsertionSort.java
@@ -2,13 +2,19 @@ public class InsertionSort {
 
   public static void sort(int array[]) {
     int n = array.length;
+    //Outer loop
     for (int j = 1; j < n; j++) {
       int key = array[j];
       int i = j - 1;
+      // when i is a positive value {array indexes can't be negative}
+      // and when current element of the first loop is less than the
+      // previous element of the first loop
+      // then assign the previous element to the current index
       while ((i > -1) && (array[i] > key)) {
         array[i + 1] = array[i];
         i--;
       }
+      // Otherwise, let the current element be the current element
       array[i + 1] = key;
     }
   }
@@ -20,9 +26,8 @@ public class InsertionSort {
       System.out.print(i + " ");
     }
     System.out.println();
-
-    sort(arr1); //sorting array using insertion sort
-
+    //Sort
+    sort(arr1);
     System.out.println("After Insertion Sort");
     for (int i : arr1) {
       System.out.print(i + " ");
