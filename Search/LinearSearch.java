5ba6b1846f9ac77bdda3512547017c3a9943e955
hezronkimutai <hezronchelimo.hc@gmail.com>
2021-01-05 21:52:47 +0300

    java: implement linear search

---
+++ Search/LinearSearch.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class LinearSearch {
+
+  private static int index = 0;
+
+  public static void main(String[] args) {
+    int[] arr = { 5, 78, 65, 89, 56, 23, 100 };
+    search(arr, 56);
+    System.out.println("56 is at index");
+    System.out.println(index);
+  }
+
+  private static void search(int[] array, int search) {
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] == search) {
+        index = i;
+      }
+    }
+  }
+}
