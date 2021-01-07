0b4ccddd71f92f37c679e36b920327ade8089df6
hezronkimutai <hezronchelimo.hc@gmail.com>
2021-01-07 16:43:47 +0300

    java:radix

--- sortingAlgorthms/Radix/RadixSort.java
+++ sortingAlgorthms/Radix/RadixSort.java
@@ -1,70 +1,52 @@
 import java.util.*;
-import java.util.Collections;
 
-class HeapSort {
-
-  static void swap(int[] a, int i, int j) {
-    int tmp = a[i];
-    a[i] = a[j];
-    a[j] = tmp;
-  }
-
-  static void max_heapify(int[] arr, int index, int arrLength) {
-    int left = 2 * index;
-    int right = 2 * index + 1;
-    int maximum;
-    // skip the block if right or left is greater than or equal to the length of  the array
-    if (right < arrLength) {
-      if (arr[left] >= arr[right]) {
-        maximum = left;
-      } else {
-        maximum = right;
-      }
-    } else if (left < arrLength) {
-      maximum = left;
-    } else return;
-    // When the node is less than the the value at index [maximum]
-    //swap the positions
-    // recursively call the functions to confirm the tree
-    if (arr[index] < arr[maximum]) {
-      swap(arr, index, maximum);
-      max_heapify(arr, maximum, arrLength);
-    }
-    return;
-  }
-
-  static void heapSort(int arr[]) {
-    int arrLength = arr.length;
-    //In the tree the node will have childrens
-    // this for loop handles  all nodes with children
-    for (int index = (int) (arrLength / 2) - 1; index >= 0; index--) {
-      max_heapify(arr, index, arrLength);
-    }
-    // Some nodes won't have children, these nodes will be handled by this loop
-    // first swap
-    for (int index = arrLength - 1; index >= 0; index--) {
-      swap(arr, 0, index);
-      max_heapify(arr, 0, index);
-    }
-  }
+public class RadixSort {
 
   public static void main(String[] args) {
-    Scanner commandReader = new Scanner(System.in);
-    System.out.println(
-      "Welcome to Java Program to perform insertion sort on int array"
-    );
-    System.out.println("Enter total number of elements : ");
-    int length = commandReader.nextInt();
-    int[] arr = new int[length];
-    System.out.printf("Enter %d integers %n", length);
-    for (int i = 0; i < length; i++) {
-      arr[i] = commandReader.nextInt();
+    int i;
+    Scanner sc = new Scanner(System.in);
+    int[] a = { 90, 23, 101, 45, 65, 23, 67, 89, 34, 23 };
+    System.out.println("\n The sorted array is: \n");
+    for (i = 0; i < 10; i++) System.out.println(a[i]);
+    radix_sort(a);
+    System.out.println("\n The sorted array is: \n");
+    for (i = 0; i < 10; i++) System.out.println(a[i]);
+  }
+
+  static int largest(int a[]) {
+    int larger = a[0], i;
+    for (i = 1; i < 10; i++) {
+      if (a[i] > larger) larger = a[i];
     }
-    heapSort(arr);
-    System.out.println("\nSorted array");
-    for (int i = 0; i < arr.length; i++) {
-      System.out.println(arr[i] + "");
+    return larger;
+  }
+
+  static void radix_sort(int a[]) {
+    int bucket[][] = new int[10][10];
+    int bucket_count[] = new int[10];
+    int i, j, k, remainder, NOP = 0, divisor = 1, larger, pass;
+    larger = largest(a);
+    while (larger > 0) {
+      NOP++;
+      larger /= 10;
     }
-    commandReader.close();
+    for (pass = 0; pass < NOP; pass++) { // Initialize the buckets
+      for (i = 0; i < 10; i++) bucket_count[i] = 0;
+      for (i = 0; i < 10; i++) {
+        // sort the numbers according to the digit at passth place
+        remainder = (a[i] / divisor) % 10;
+        bucket[remainder][bucket_count[remainder]] = a[i];
+        bucket_count[remainder] += 1;
+      }
+      // collect the numbers after PASS pass
+      i = 0;
+      for (k = 0; k < 10; k++) {
+        for (j = 0; j < bucket_count[k]; j++) {
+          a[i] = bucket[k][j];
+          i++;
+        }
+      }
+      divisor *= 10;
+    }
   }
 }
