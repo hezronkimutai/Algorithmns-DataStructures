3840bbac16dd0692f5251a6cfc8a0445ebc72e33
hezronkimutai <hezronchelimo.hc@gmail.com>
2021-01-08 08:58:31 +0300

    radix sort docs

--- sortingAlgorthms/Radix/RadixSort.java
+++ sortingAlgorthms/Radix/RadixSort.java
@@ -1,6 +1,58 @@
+// Radix sort Java implementation
+import java.io.*;
 import java.util.*;
 
-public class RadixSort {
+class RadixSort {
+
+  // A utility function to get maximum value in arr[]
+  static int getMax(int arr[], int n) {
+    int mx = arr[0];
+    for (int i = 1; i < n; i++) if (arr[i] > mx) mx = arr[i];
+    return mx;
+  }
+
+  // A function to do counting sort of arr[] according to
+  // the digit represented by exp.
+  static void countSort(int arr[], int n, int exp) {
+    int output[] = new int[n]; // output array
+    int i;
+    int count[] = new int[10];
+    Arrays.fill(count, 0);
+
+    // Store count of occurrences in count[]
+    for (i = 0; i < n; i++) count[(arr[i] / exp) % 10]++;
+
+    // Change count[i] so that count[i] now contains
+    // actual position of this digit in output[]
+    for (i = 1; i < 10; i++) count[i] += count[i - 1];
+
+    // Build the output array
+    for (i = n - 1; i >= 0; i--) {
+      output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+      count[(arr[i] / exp) % 10]--;
+    }
+
+    // Copy the output array to arr[], so that arr[] now
+    // contains sorted numbers according to curent digit
+    for (i = 0; i < n; i++) arr[i] = output[i];
+  }
+
+  // The main function to that sorts arr[] of size n using
+  // Radix Sort
+  static void radixsort(int arr[], int n) {
+    // Find the maximum number to know number of digits
+    int m = getMax(arr, n);
+
+    // Do counting sort for every digit. Note that
+    // instead of passing digit number, exp is passed.
+    // exp is 10^i where i is current digit number
+    for (int exp = 1; m / exp > 0; exp *= 10) countSort(arr, n, exp);
+  }
+
+  // A utility function to print an array
+  static void print(int arr[], int n) {
+    for (int i = 0; i < n; i++) System.out.print(arr[i] + " ");
+  }
 
   public static void main(String[] args) {
     Scanner commandReader = new Scanner(System.in);
@@ -14,46 +66,13 @@ public class RadixSort {
     for (int i = 0; i < length; i++) {
       arr[i] = commandReader.nextInt();
     }
-    radix_sort(arr);
+    int n = arr.length;
+
+    radixsort(arr, n);
     System.out.println("\nSorted array");
     for (int i = 0; i < arr.length; i++) {
       System.out.println(arr[i] + "");
     }
     commandReader.close();
-  }
-
-  static int largest(int a[]) {
-    int larger = a[0], i;
-    for (i = 1; i < 10; i++) {
-      if (a[i] > larger) larger = a[i];
-    }
-    return larger;
-  }
-
-  static void radix_sort(int a[]) {
-    int bucket[][] = new int[10][10];
-    int bucket_count[] = new int[10];
-    int i, j, k, remainder, NOP = 0, divisor = 1, larger, pass;
-    larger = largest(a);
-    while (larger > 0) {
-      NOP++;
-      larger /= 10;
-    }
-    for (pass = 0; pass < NOP; pass++) {
-      for (i = 0; i < 10; i++) bucket_count[i] = 0;
-      for (i = 0; i < 10; i++) {
-        remainder = (a[i] / divisor) % 10;
-        bucket[remainder][bucket_count[remainder]] = a[i];
-        bucket_count[remainder] += 1;
-      }
-      i = 0;
-      for (k = 0; k < 10; k++) {
-        for (j = 0; j < bucket_count[k]; j++) {
-          a[i] = bucket[k][j];
-          i++;
-        }
-      }
-      divisor *= 10;
-    }
   }
 }
